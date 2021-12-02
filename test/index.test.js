4d69b4d66b298aef03d6c14e9abcf08d6eaed12d
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2021-12-02 16:10:59 +0800

    run eslint script

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
-import { mergeList } from "index";
+import { mergeList } from 'index';
 
-test("merged the intervals", () => {
-  const list = [
-    [-50, 2],
-    [2, 4],
-    [5, 8],
-  ];
-  const result = [
-    [-50, 4],
-    [5, 8],
-  ];
-  expect(mergeList(list)).toEqual(result);
+test('merged the intervals', () => {
+	const list = [
+		[-50, 2],
+		[2, 4],
+		[5, 8],
+	];
+	const result = [
+		[-50, 4],
+		[5, 8],
+	];
+	expect(mergeList(list)).toEqual(result);
 });
