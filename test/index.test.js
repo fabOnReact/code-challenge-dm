f67c16428a599188caead544359775abb9ad5d49
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2021-12-03 12:16:07 +0800

    adding jest tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,69 @@
-import { mergeList } from 'index';
+import { mergeList, getMessage } from "index";
 
-test('merged the intervals', () => {
-  const list = [
-    [-50, 2],
-    [2, 4],
-    [5, 8],
-  ];
-  const result = [
-    [-50, 4],
-    [5, 8],
-  ];
-  expect(mergeList(list)).toEqual(result);
+describe("merges a list", () => {
+  test("with negative numbers", () => {
+    const list = [
+      [-50, 2],
+      [2, 4],
+      [5, 8],
+    ];
+    const result = [
+      [-50, 4],
+      [5, 8],
+    ];
+    expect(mergeList(list)).toEqual(result);
+  });
+
+  test("that is an empty array", () => {
+    expect(mergeList([])).toEqual([]);
+  });
+
+  test("with duplicates", () => {
+    const list = [
+      [2, 4],
+      [2, 4],
+    ];
+    expect(mergeList(list)).toEqual([[2, 4]]);
+  });
+});
+
+describe("throws an error when", () => {
+  test("the first interval value is invalid", () => {
+    expect(() =>
+      mergeList([
+        [undefined, 1],
+        [1, 2],
+      ])
+    ).toThrow(getMessage("WRONG_LIST_VALUE", [undefined, 1]));
+  });
+
+  test("the second interval value is invalid", () => {
+    expect(() =>
+      mergeList([
+        [1, 1],
+        [2, null],
+      ])
+    ).toThrow(getMessage("WRONG_LIST_VALUE", [2, null]));
+  });
+
+  test("the interval start minute is after the end minute", () => {
+    expect(() =>
+      mergeList([
+        [1, 1],
+        [2, 1],
+      ])
+    ).toThrow(getMessage("WRONG_INTERVAL_START", [2, 1]));
+  });
+
+  test("the list is undefined", () => {
+    expect(() => mergeList(undefined)).toThrow(
+      getMessage("WRONG_LIST_VALUE", undefined)
+    );
+  });
+
+  test("the list is not an Array", () => {
+    expect(() => mergeList("word")).toThrow(
+      getMessage("WRONG_LIST_TYPE", "word")
+    );
+  });
 });
